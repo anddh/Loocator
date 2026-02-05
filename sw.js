4446502bfa97074bc433ac253df4baf5dbedf999
anddh <foofty@gmail.com>
2026-02-05 18:33:02 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'loocator-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
